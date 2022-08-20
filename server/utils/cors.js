794dd3baf8250e22f6888bea2233f69ef7dd82c5
Avninder Preet Singh <avninderpreet99@gmail.com>
2022-08-21 00:15:02 +0530

    Update cors.js

--- server/utils/cors.js
+++ server/utils/cors.js
@@ -1,8 +1,7 @@
 const corsOptions = {
   cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"],
-    credentials: true,
+    origin: "*",
+    methods: ["GET", "POST"]
   },
 };
 
